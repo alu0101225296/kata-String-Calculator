0951fb965e723ee732a4ab46496f9177e21650c5
alu0101225296 <alu0101225296@ull.edu.es>
2022-04-01 10:56:52 +0100

    Test fail: should_return_number_if_there_is_only_on

--- stringCalculator/src/main/java/StringCalculator/StringCalculator.java
+++ stringCalculator/src/main/java/StringCalculator/StringCalculator.java
@@ -1,4 +1,7 @@
 package StringCalculator;
 
 public class StringCalculator {
+    public int sum(String s) {
+        return 0;
+    }
 }
--- stringCalculator/src/test/java/StringCalculator/StringCalculatorTest.java
+++ stringCalculator/src/test/java/StringCalculator/StringCalculatorTest.java
@@ -5,7 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
 
+    // "" -> 0
+    // "1" -> 1
+    // "1,3" -> 4
+    // "1," -> ???
+    // "1,asdf" -> ???
+
+    
     @Test
-    void initializeFizz() {
+    void should_return_number_if_there_is_only_one() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.sum("1"), "1"); 
     }
 };
