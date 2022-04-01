34ef4ca77c6ac526f49f64067bb9abfb14e804ad
alu0101225296 <alu0101225296@ull.edu.es>
2022-04-01 11:05:01 +0100

    Test pass: should_return_zero_if_empty

--- stringCalculator/src/main/java/StringCalculator/StringCalculator.java
+++ stringCalculator/src/main/java/StringCalculator/StringCalculator.java
@@ -2,6 +2,9 @@ package StringCalculator;
 
 public class StringCalculator {
     public int sum(String numberList) {
+        if (numberList.isEmpty()) {
+            return 0;
+        }
         return Integer.parseInt(numberList);
     }
 }
