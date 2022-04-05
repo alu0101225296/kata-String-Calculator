38774d4bec7516c702e010aff2721a4aa5e32869
alu0101225296 <alu0101225296@ull.edu.es>
2022-04-05 14:22:22 +0100

    Pass: should_be_able_to_add_multiple_number

--- TypeScript/src/StringCalculator.ts
+++ TypeScript/src/StringCalculator.ts
@@ -5,12 +5,22 @@ export class StringCalculator {
 
   public sum(expression: string): number {
     const delimiterRegex = new RegExp(',');
-    const numberRegex = new RegExp('\d+'); 
-    const notAllowedCharacters = new RegExp(`[^${numberRegex}${delimiterRegex}]`);
+    //const numberRegex = new RegExp('\d+'); 
+    //const notAllowedCharacters = new RegExp(`[^${numberRegex}${delimiterRegex}]`);
 
     expression = expression.replace(/\s+/,"");
-    if(expression.match(/[^\d+,]/))
+    if(expression.match(/[^\d+,]/)) // cambiar por notallowed cuando lo arregle
       throw new Error('Unsupported input');
-    return Number(expression);
+
+
+    const expressionArray : string[] = expression.split(delimiterRegex);
+
+    if(expressionArray.length <= 1) {
+      return Number(expression);
+    } else {
+      const expressionSimple : string = expressionArray.pop();
+      expression = expressionArray.join();
+      return (this.sum(expression) + this.sum(expressionSimple));
+    }
   }
 }
