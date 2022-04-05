a9dacf47efef14a35c2873e6eaf99569e0c6e133
alu0101225296 <alu0101225296@ull.edu.es>
2022-04-05 14:37:17 +0100

    Refactor

--- TypeScript/src/StringCalculator.ts
+++ TypeScript/src/StringCalculator.ts
@@ -4,18 +4,18 @@
 export class StringCalculator {
 
   public sum(expression: string): number {
-    const delimiterRegex = new RegExp(',');
-    //const numberRegex = new RegExp('\d+'); 
-    //const notAllowedCharacters = new RegExp(`[^${numberRegex}${delimiterRegex}]`);
+    const delimiterRegex = ',';
+    const numberRegex = '\\d+'; 
+    const notAllowedCharacters = new RegExp(`[^${numberRegex}${delimiterRegex}]`);
 
     expression = expression.replace(/\s+/,"");
-    if(expression.match(/[^\d+,]/)) // cambiar por notallowed cuando lo arregle
+    if(expression.match(notAllowedCharacters))
       throw new Error('Unsupported input');
 
 
     const expressionArray : string[] = expression.split(delimiterRegex);
 
-    if(expressionArray.length <= 1) {
+    if(expressionArray.length == 1) {
       return Number(expression);
     } else {
       const expressionSimple : string = expressionArray.pop();
