6ec2436ca1aecd66bf247dac6f63b575cce78627
alu0101225296 <alu0101225296@ull.edu.es>
2022-04-05 18:26:32 +0100

    Pass: allow_different_delimiters

--- TypeScript/src/StringCalculator.ts
+++ TypeScript/src/StringCalculator.ts
@@ -4,11 +4,17 @@
 export class StringCalculator {
 
   public sum(expression: string): number {
-    const delimiterRegex = ',|\\n';
+    let delimiterRegex = ',|\\n';
     const numberRegex = '\\d+'; 
-    const notAllowedCharacters = new RegExp(`[^${numberRegex}${delimiterRegex}]`);
 
-    if(expression.match(notAllowedCharacters))
+    if(expression.startsWith('//')) {
+      const definitionEnd = expression.indexOf('\n');
+      delimiterRegex += `|[${expression.slice(2, definitionEnd)}]`;
+      expression = expression.slice(definitionEnd + 1);
+    }
+
+    const simpleExpressionRegex = `(^(${numberRegex}(${delimiterRegex})?)+$)|(^$)`;
+    if(!new RegExp(simpleExpressionRegex).test(expression)) 
       throw new Error('Unsupported input');
 
     const expressionArray : string[] = expression.split(new RegExp(delimiterRegex));
