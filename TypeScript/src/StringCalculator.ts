b55c42c6fb6307d44d1da24032643d5b7952fe04
alu0101225296 <alu0101225296@ull.edu.es>
2022-04-07 12:25:26 +0100

    Added README.md

--- TypeScript/src/StringCalculator.ts
+++ TypeScript/src/StringCalculator.ts
@@ -4,53 +4,53 @@
 export class StringCalculator {
   private negativeRegex = /[-]\d+/g;
   private numberRegex = '\\d+';
-  
-  public sum(expression: string): number {
 
+  public sum(expression: string): number {
     const expressionArray = this.processExpression(expression);
-    
-    if(expressionArray.length <= 1) {
-      const result = Number(expression); 
-      return (result < 1000) ? Number(expression) : 0;
+
+    if (expressionArray.length <= 1) {
+      const result = Number(expression);
+      return result < 1000 ? Number(expression) : 0;
     } else {
-      const expressionSimple : string = expressionArray.pop();
+      const expressionSimple: string = expressionArray.pop();
       expression = expressionArray.join();
-      return (this.sum(expression) + this.sum(expressionSimple));
+      return this.sum(expression) + this.sum(expressionSimple);
     }
   }
 
-  private processExpression(expression: string): string[] {   
+  private processExpression(expression: string): string[] {
     let delimiterRegex = ',|\\n';
-     
-    if(expression.startsWith('//')) { 
+
+    if (expression.startsWith('//')) {
       const definitionEnd = expression.indexOf('\n');
       const newDelimiter = expression.slice(2, definitionEnd);
       expression = expression.slice(definitionEnd + 1);
 
-      if(newDelimiter.length == 1) 
-        delimiterRegex += `|[${newDelimiter}]`;
-        else 
-        delimiterRegex += '|' + this.getDelimiters(newDelimiter.match(/\[.*?\]/g)).join('|');
+      delimiterRegex +=
+        newDelimiter.length == 1
+          ? `|[${newDelimiter}]`
+          : '|' + this.getDelimiters(newDelimiter.match(/\[.*?\]/g)).join('|');
     }
 
     const simpleExpressionRegex = `(^(${this.numberRegex}(${delimiterRegex})?)+$)|(^$)`;
-    if(this.negativeRegex.test(expression)) { 
-      throw new Error(`Negatives not allowed: ${expression.match(this.negativeRegex)}`);
-    } else if(!new RegExp(simpleExpressionRegex).test(expression)) {
+    if (this.negativeRegex.test(expression)) {
+      throw new Error(
+        `Negatives not allowed: ${expression.match(this.negativeRegex)}`,
+      );
+    } else if (!new RegExp(simpleExpressionRegex).test(expression)) {
       throw new Error('Unsupported input');
     }
-       
-    return expression.split(new RegExp(delimiterRegex));
-  } 
 
-  private getDelimiters(delimiterArray : string[]) : string[] {
-    if(delimiterArray.length == 1) {
+    return expression.split(new RegExp(delimiterRegex));
+  }
+
+  private getDelimiters(delimiterArray: string[]): string[] {
+    if (delimiterArray.length == 1) {
       const delimiter = delimiterArray[0].slice(1, -1);
       return [delimiter.replace(/(.{1})/g, '[\\$1]')];
-    } else {
-      return (this.getDelimiters([delimiterArray.pop()])).concat(this.getDelimiters(delimiterArray));
     }
+    return this.getDelimiters([delimiterArray.pop()]).concat(
+      this.getDelimiters(delimiterArray),
+    );
   }
 }
-
-
