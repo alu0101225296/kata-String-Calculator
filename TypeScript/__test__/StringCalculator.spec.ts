16bf04710bb7ba4441bc5196ef66c2f996f3cc0d
alu0101225296 <alu0101225296@ull.edu.es>
2022-04-07 18:44:27 +0100

    Updated tests name

--- TypeScript/__test__/StringCalculator.spec.ts
+++ TypeScript/__test__/StringCalculator.spec.ts
@@ -1,24 +1,24 @@
 import { StringCalculator } from '../src/StringCalculator';
 
-describe('Basic add test', () => {
+describe('Sum function should', () => {
   const calculator = new StringCalculator();
 
-  it('should_return_number_if_there_is_only_one', () => {
+  it('return_number_if_there_is_only_one', () => {
     expect(calculator.sum('1')).toBe(1);
     expect(calculator.sum('4')).toBe(4);
   });
 
-  it('should_return_zero_if_empty', () => {
+  it('return_zero_if_empty', () => {
     expect(calculator.sum('')).toBe(0);
   });
 
-  it('should_throw_error_if_not_allowed_character', () => {
+  it('throw_error_if_not_allowed_character', () => {
     expect(() => calculator.sum('ABC')).toThrowError('Unsupported input');
     expect(() =>calculator.sum('ABC,<')).toThrowError('Unsupported input');
     expect(() =>calculator.sum('A,C,1')).toThrowError('Unsupported input');
   });
 
-  it('should_be_able_to_add_multiple_number', () => {
+  it('be_able_to_add_multiple_number', () => {
     expect(calculator.sum('1,1')).toBe(2);
     expect(calculator.sum('3,6,2')).toBe(11);
     expect(calculator.sum('8,20,12,4,6')).toBe(50);
@@ -68,6 +68,7 @@ describe('Basic add test', () => {
     expect(calculator.sum('//[****][%][^^^]\n20^^^12****4,6%1000')).toBe(42);
     expect(calculator.sum('//[;;]\n2;;1001')).toBe(2);
     expect(calculator.sum('//[||||][__]\n2__45||||9__3')).toBe(59);
+    expect(() =>calculator.sum('//:\n2::2')).toThrowError('Unsupported input');  
   });
 
   it('negative_number_will_throw_an_exception', () => {
